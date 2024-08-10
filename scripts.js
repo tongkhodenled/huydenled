c581c197f059fe049d439bb44df9fb0d51698a57
denledtrangia <quanghuydtvt@gmail.com>
2024-08-10 13:56:03 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Cảm ơn bạn đã liên hệ! Chúng tôi sẽ sớm phản hồi.');
+    // Xử lý form liên hệ ở đây (gửi email, lưu trữ dữ liệu, ...)
+});
